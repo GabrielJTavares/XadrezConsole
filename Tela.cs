0d30df599d2c2b24a629259e8731baa6c14581f7
GabrielJTavares <gabriel-tavares1996@outlook.com>
2020-11-06 10:21:29 -0300

    Xadrez completo

---
+++ Tela.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+using jogo;
+
+namespace Xadrez
+{
+    class Tela
+    {
+        public static void imprimirPartida(PartidaXadrez partida)
+        {
+            imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.turno);
+            if (!partida.terminada)
+            {
+                Console.WriteLine("Aguardando a jogada da peça: " + partida.jogadorAtual);
+
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: "+partida.jogadorAtual);
+            }
+
+        }
+
+        public static void imprimirPecasCapturadas(PartidaXadrez partida)
+        {
+            Console.WriteLine("Peças Capturadas: ");
+            Console.Write("Brancas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.WriteLine();
+            Console.Write("Pretas:  ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.preta));
+            Console.WriteLine();
+        }
+
+        public static void imprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[ ");
+            foreach(Peca x in conjunto)
+            {
+                ConsoleColor origins = Console.ForegroundColor;
+                ConsoleColor alt = ConsoleColor.Red;
+                if (x.cor == Cor.preta)
+                { 
+                    Console.ForegroundColor = alt;
+                }
+                
+               
+                Console.Write(x + " ");
+                Console.ForegroundColor = origins;
+            }
+            Console.Write(" ]");
+
+        }
+
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                ConsoleColor x = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(8 - i + " ");
+                Console.ForegroundColor = x;
+
+                for (int j = -0; j < tab.colunas; j++)
+                {
+
+
+                    imprimirPeca(tab.peca(i, j));
+
+
+
+                }
+                Console.WriteLine();
+            }
+
+            ConsoleColor z = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("  a b c d e f g h");
+            Console.ForegroundColor = z;
+
+
+        }
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
+        {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                ConsoleColor x = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(8 - i + " ");
+                Console.ForegroundColor = x;
+
+                for (int j = -0; j < tab.colunas; j++)
+                {
+                    if (posicoesPossiveis[i, j])
+                    {
+                        Console.BackgroundColor = fundoAlterado;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+
+
+
+                    imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
+
+
+
+                }
+                Console.WriteLine();
+            }
+
+            ConsoleColor z = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("  a b c d e f g h");
+            Console.BackgroundColor = fundoOriginal;
+            Console.ForegroundColor = z;
+            Console.BackgroundColor = fundoOriginal;
+
+
+        }
+
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+
+            if (peca == null)
+            {
+                Console.Write("- ");
+            }
+            else
+            {
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
+            }
+        }
+
+
+    }
+}
---
+++ jogo/Rei.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace jogo
+{
+    class Rei : Peca
+    {
+        private PartidaXadrez partida;
+        public Rei(Tabuleiro tab, Cor cor, PartidaXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && qtdMovimentos == 0;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //nordeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //sudeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //sudoeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            //noroeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //jogadaespecial roque
+            if(qtdMovimentos==0&& !partida.xeque)
+            {
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                //#jogadaespecial Roque pequeno
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tab.peca(p1)==null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+
+                }
+
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna -4);
+                //#jogadaespecial Roque grande
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna -3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null&&tab.peca(p3)==null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+
+                }
+            }
+
+
+            return mat;
+
+        }
+
+    }
+}
